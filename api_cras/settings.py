074e916c899c7ac412fbfaf48a032f6af9e8c4d3
Gustavo Tavares <gustavotavaresgtm11@gmail.com>
2025-09-16 22:43:30 -0300

    requirements adicionado

--- api_cras/settings.py
+++ api_cras/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ou8zs)i%urkovir+5970&(9xqneea_n!0*wmxi8@(z5_k&tgc%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

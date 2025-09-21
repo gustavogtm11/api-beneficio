3b4e64fe0ddbd78a799343801f7ac6591d88438e
Gustavo Tavares <gustavotavaresgtm11@gmail.com>
2025-09-21 14:14:39 -0300

    update

--- system/urls.py
+++ system/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("beneficiarios/", views.beneficiarios_view, name="beneficiarios"),
     path("cadastro/", views.cadastro_view, name="cadastro"),
+    path("logout/", LogoutView.as_view(next_page=reverse_lazy('login')), name="logout"),
 ]
